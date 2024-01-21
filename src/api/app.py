95778348f39da0f6a802bd96c3aa4b206fd89586
Ayokunle Adeniyi <Ayokunle.Adeniyi@warwick.ac.uk>
2024-01-21 20:03:53 +0000

    renamed server.py to app.py, added ingest time and client ip to each event

--- src/api/app.py
+++ src/api/app.py
@@ -48,8 +48,8 @@ def publish_message():
                 "error": error,
                 "timestamp": datetime.datetime.now().timestamp()
             }
-            publisher.publish(invalid_topic_path, data=json.dumps(invalid_data).encode('utf-8'))
             logging.error(error)
+            publisher.publish(invalid_topic_path, data=json.dumps(invalid_data).encode('utf-8'))
             return jsonify({'error': error}), 400
 
         if 'data' not in body:
@@ -63,21 +63,37 @@ def publish_message():
             logging.error(error)
             return jsonify({'error': error}), 400
 
+        logging.debug(request.headers)
+        # add receive timestamp
+        body['data']['receive-timestamp'] = datetime.datetime.now().timestamp()
+        # get client IP
+        client_ip = None
+        if ('X-Forwarded-For' in request.headers.keys()):
+            client_ip = request.headers['X-Forwarded-For']
+        # add client IP to message body
+        body['data']['client_ip'] = client_ip
 
+        # troubleshoot
+        logging.debug(f"data: {body}")
         # Publish the message to Pub/Sub
         future = publisher.publish(valid_topic_path, data=json.dumps(body).encode())
         message_id = future.result()
 
         return jsonify({'message_id': message_id}), 200
+        # return jsonify({'message': body}), 200
 
     except NotFound as nf:
         logging.exception(f"Exception raised: resource not found {nf}")
     except Exception as e:
         logging.exception(f"Exception Raised: {e}")
 
+
 @app.route('/publish', methods=['GET'])
 def publish_message_get():
     return "publish api"
 
+
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    # app.run(debug=True, port=8080)
+    app.run(port=int(os.environ.get("PORT", 8080)), host='0.0.0.0', debug=True)
+    # app.run(debug=False, port=8080)
